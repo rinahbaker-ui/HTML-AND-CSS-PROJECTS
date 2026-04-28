7bdc5f4cec65d611dd0e01dfcdca92db1fc5187b
Barbara <shopgirl.online.75@gmail.com>
2026-04-28 14:21:17 -0700

    commit

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <body>
+
+    <button onclick="My_First_Function()">
+
+    Click Me!
+
+    </button>
+
+    <p id="I'm Irish"></p>
+<script>
+
+    Function My_First_Function() {
+
+        var String = "Kiss me, I'm Irish";
+        var result = string.fontcolor("green");
+        document.getElementById("Irish").innerHTML = result;
+
+    }
+</script>
+
+
+
+
+
+
+
+
+    </body>
+</html>
